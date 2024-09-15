36c21fc61ec741312cf472f1538ba6c70e45e0c8
DURLOV <bormondurlov@gmail.com>
2024-09-15 15:26:30 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var typed = new Typed(".typewriter-text ",
+    {
+        strings: [" ",
+            "Student",
+
+            "Developer",
+
+            "Theoretical Thinker",
+
+            "DMC - Dreamer",
+
+        ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop:true,
+
+
+    }
+)
